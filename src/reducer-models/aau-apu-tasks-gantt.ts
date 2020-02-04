ab94562b55230021d293096b5ee91a81904bfa1e
Andres Rios <rianby64@gmail.com>
2020-02-04 12:58:06 +0300

    [NO-TEST] Finalice AAU-APU-Tasks-Gantt

--- src/reducer-models/aau-apu-tasks-gantt.ts
+++ src/reducer-models/aau-apu-tasks-gantt.ts
@@ -3,15 +3,38 @@ import { State, Row, PK, AAUAPUTasksGantt } from '../types';
 type TRow = AAUAPUTasksGantt['Row'];
 type TAgg = AAUAPUTasksGantt['Agg'];
 
-export function Select(_: State, rows: Row[]): { Rows: TRow[]; Aggs: TAgg[] } {
-  const Rows = rows as TRow[];
-  return {
-    Rows: Rows,
-    Aggs: Rows.reduce((acc, row) => {
-      let curr = acc.find(agg =>
-        (agg.Key === row.Key) && (agg.Constraint === row.Constraint));
-      if (curr) {
-        curr.APU.push({
+function ReduceRows(Rows: TRow[]) {
+  return Rows.reduce((acc, row) => {
+    let curr = acc.find(agg =>
+      (agg.Key === row.Key) && (agg.Constraint === row.Constraint));
+    if (curr) {
+      curr.APU.push({
+        ID:           row['APU-ID'],
+        ContractorID: row.ContractorID,
+        Status:       row.Status,
+        Frozen:       row.Frozen,
+        ExpiredAt:    row.ExpiredAt,
+        Description:  row['APU-Description'],
+        Unit:         row['APU-Unit'],
+        P:            row['APU-P'],
+        Q:            row['APU-Q'],
+        T:            row['APU-T'],
+        Start:        row['APU-Start'],
+        End:          row['APU-End'],
+      });
+    } else {
+      acc.push({
+        Project:     row.Project,
+        Key:         row.Key,
+        Constraint:  row.Constraint,
+        Description: row['AAU-Description'],
+        Unit:        row['AAU-Description'],
+        P:           row['AAU-P'],
+        Q:           row['AAU-Q'],
+        T:           row['AAU-T'],
+        Start:       row['AAU-Start'],
+        End:         row['AAU-End'],
+        APU: [{
           ID:           row['APU-ID'],
           ContractorID: row.ContractorID,
           Status:       row.Status,
@@ -24,47 +47,26 @@ export function Select(_: State, rows: Row[]): { Rows: TRow[]; Aggs: TAgg[] } {
           T:            row['APU-T'],
           Start:        row['APU-Start'],
           End:          row['APU-End'],
-        });
-      } else {
-        acc.push({
-          Project:     row.Project,
-          Key:         row.Key,
-          Constraint:  row.Constraint,
-          Description: row['AAU-Description'],
-          Unit:        row['AAU-Description'],
-          P:           row['AAU-P'],
-          Q:           row['AAU-Q'],
-          T:           row['AAU-T'],
-          Start:       row['AAU-Start'],
-          End:         row['AAU-End'],
-          APU: [{
-            ID:           row['APU-ID'],
-            ContractorID: row.ContractorID,
-            Status:       row.Status,
-            Frozen:       row.Frozen,
-            ExpiredAt:    row.ExpiredAt,
-            Description:  row['APU-Description'],
-            Unit:         row['APU-Unit'],
-            P:            row['APU-P'],
-            Q:            row['APU-Q'],
-            T:            row['APU-T'],
-            Start:        row['APU-Start'],
-            End:          row['APU-End'],
-          }]
-        });
-      }
-      return acc;
-    }, [] as TAgg[])};
+        }]
+      });
+    }
+    return acc;
+  }, [] as TAgg[]);
 }
 
-export function Insert(state: State, rows: Row[], row: Row, pk?: PK): { Rows: TRow[]; Aggs: TAgg[] } {
-  const Row = row as TRow;
+export function Select(_: State, rows: Row[]): { Rows: TRow[]; Aggs: TAgg[] } {
   const Rows = rows as TRow[];
-  const PK = pk || { Key: Row.Key, Constraint: Row.Constraint, 'APU-ID': Row['APU-ID'] };
-  const keys = Object.keys(PK) as (keyof typeof PK)[];
   return {
-    Rows: [Row, ...Rows],
-    Aggs: []
+    Rows: Rows,
+    Aggs: ReduceRows(Rows)};
+}
+
+export function Insert(_: State, rows: Row[], row: Row, _pk?: PK): { Rows: TRow[]; Aggs: TAgg[] } {
+  const Row = row as TRow;
+  const Rows = [Row, ...rows as TRow[]];
+  return {
+    Rows: Rows,
+    Aggs: ReduceRows(Rows),
   };
 }
 
@@ -72,11 +74,12 @@ export function Update(state: State, row: Row, pk?: PK): { Rows: TRow[]; Aggs: T
   const Row = row as TRow;
   const PK = pk || { Key: Row.Key, Constraint: Row.Constraint, 'APU-ID': Row['APU-ID'] };
   const keys = Object.keys(PK) as (keyof typeof PK)[];
+  const Rows = state.Source['AAU-APU-Tasks-Gantt'].Rows
+    .map((row): TRow =>
+      (keys.every((key): boolean => PK[key] === row[key])) ? Row : row);
   return {
-    Rows: state.Source['AAU-APU-Tasks-Gantt'].Rows
-      .map((row): TRow =>
-        (keys.every((key): boolean => PK[key] === row[key])) ? Row : row),
-    Aggs: []
+    Rows: Rows,
+    Aggs: ReduceRows(Rows),
   };
 }
 
@@ -84,10 +87,11 @@ export function Delete(state: State, row: Row, pk?: PK): { Rows: TRow[]; Aggs: T
   const Row = row as TRow;
   const PK = pk || { Key: Row.Key, Constraint: Row.Constraint, 'APU-ID': Row['APU-ID'] };
   const keys = Object.keys(PK) as (keyof typeof PK)[];
+  const Rows = state.Source['AAU-APU-Tasks-Gantt'].Rows
+    .filter((row): boolean =>
+      (keys.every((key): boolean => PK[key] === row[key])) ? false : true);
   return {
-    Rows: state.Source['AAU-APU-Tasks-Gantt'].Rows
-      .filter((row): boolean =>
-        (keys.every((key): boolean => PK[key] === row[key])) ? false : true),
-    Aggs: []
+    Rows: Rows,
+    Aggs: ReduceRows(Rows),
   };
 }
