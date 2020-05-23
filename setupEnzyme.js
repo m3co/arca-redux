44964c2f711fe2a510046089e2e6f1b95daa772f
Anton Novikau <anton.novikov.y@gmail.com>
2020-05-23 15:01:08 +0300

    Wipe out

--- setupEnzyme.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const enzyme = require('enzyme');
-const EnzymeAdapter = require('enzyme-adapter-react-16');
-enzyme.configure({ adapter: new EnzymeAdapter() });
